1371ffe4d7472668a3b4854b8e0d6b4482e7a981
dapirra <35818038+dapirra@users.noreply.github.com>
2021-02-11 03:49:06 -0500

    Refactored and moved a lot of code around in jAdvise.java

--- src/main/java/jadvise/gui/JAdvise.java
+++ src/main/java/jadvise/gui/JAdvise.java
@@ -32,7 +32,7 @@ import javax.swing.ListSelectionModel;
  */
 public class JAdvise extends JFrame {
 
-	private final String TITLE = "JAdvise";
+	private static final String TITLE = "JAdvise";
 	private final StudentDatabase sd;
 
 	// Menu
@@ -52,13 +52,6 @@ public class JAdvise extends JFrame {
 	private final JTable table;
 	private final JScrollPane tableScrollPane;
 
-	// Search Area
-	private final JPanel searchPanel = new JPanel(new BorderLayout());
-	private final JTextField searchField;
-	private final JLabel searchLabel;
-
-	protected static Container jAdvise;
-
 	public static final String[] columnNames = {
 			"<html><b>ID Number</b></html>",
 			"<html><b>First Name</b></html>",
@@ -77,15 +70,24 @@ public class JAdvise extends JFrame {
 			"<html><b>Email Address</b></html>"
 	};
 
+	public static void resetAllColumnWidths(JTable table) {
+		for (int i = 0; i < columnWidths.length; i++) {
+			table.getColumnModel().getColumn(i).setPreferredWidth(columnWidths[i]);
+		}
+	}
+
 	public static final int[] columnWidths = {
 			100, 100, 30, 200, 40,
 			125, 60, 120, 200, 100,
 			40, 60, 175, 175, 400
 	};
 
-	public static void main(String[] args) {
-		jAdvise = new JAdvise(new MySQLAccount("root", "usbw"));
-	}
+	// Search Area
+	private final JPanel searchPanel = new JPanel(new BorderLayout());
+	private final JTextField searchField;
+	private final JLabel searchLabel;
+
+	protected static Container jAdvise;
 
 	public JAdvise(MySQLAccount account) {
 		jAdvise = getRootPane();
@@ -96,10 +98,25 @@ public class JAdvise extends JFrame {
 		setLocationRelativeTo(null);
 		setResizable(true);
 
+		// Load Data
+		sd = new StudentDatabase(account);
+		try {
+			sd.loadData();
+		} catch (com.mysql.jdbc.exceptions.jdbc4.CommunicationsException ce) {
+			ErrorMessagePane.showErrorMessage(jAdvise, "Can't connect to database.");
+		} catch (SQLException ex) {
+			ErrorMessagePane.showErrorMessage(jAdvise, "SQL Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ErrorMessagePane.showErrorMessage(jAdvise,
+					"Something went wrong that you probably won't understand (ClassNotFoundException).");
+			System.exit(1);
+		}
+
 		// MenuBar
 		menuBar = new JMenuBar();
 
-		//File Menu
+		// File Menu
 		printItem = new JMenuItem("Print");
 		printItem.setMnemonic(KeyEvent.VK_P);
 		printItem.addActionListener(new ActionListener() {
@@ -115,34 +132,28 @@ public class JAdvise extends JFrame {
 
 		clearItem = new JMenuItem("Clear");
 		clearItem.setMnemonic(KeyEvent.VK_C);
-		clearItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				if (0 == JOptionPane.showConfirmDialog(getRootPane(),
-						"Are you sure you want to clear the table?",
-						TITLE, JOptionPane.WARNING_MESSAGE,
-						JOptionPane.OK_CANCEL_OPTION)) {
-					try {
-						sd.clearData();
-						sd.updateTable();
-					} catch (SQLException ex) {
-						ex.printStackTrace();
-						System.exit(1);
-					} catch (ClassNotFoundException ex) {
-						System.exit(1);
-					}
+		clearItem.addActionListener(ae -> {
+			if (JOptionPane.showConfirmDialog(
+					getRootPane(),
+					"Are you sure you want to clear the table?",
+					TITLE,
+					JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
+				try {
+					sd.clearData();
+					sd.updateTable();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+					System.exit(1);
+				} catch (ClassNotFoundException ex) {
+					System.exit(1);
 				}
 			}
 		});
 
 		exitItem = new JMenuItem("Exit");
 		exitItem.setMnemonic(KeyEvent.VK_X);
-		exitItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				System.exit(0);
-			}
-		});
+		exitItem.addActionListener(actionEvent -> System.exit(0));
 
 		fileMenu = new JMenu("File");
 		fileMenu.setMnemonic(KeyEvent.VK_F);
@@ -150,13 +161,13 @@ public class JAdvise extends JFrame {
 		fileMenu.add(clearItem);
 		fileMenu.add(exitItem);
 
-		//Edit Menu
+		// Edit Menu
 		addStudentItem = new JMenuItem("Add Student");
 		addStudentItem.setMnemonic(KeyEvent.VK_A);
 		addStudentItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
-				AddEditStudent addEditStudent = new AddEditStudent(jAdvise, sd, null);
+				new AddEditStudent(jAdvise, sd, null);
 			}
 		});
 
@@ -168,14 +179,9 @@ public class JAdvise extends JFrame {
 				if (table.getSelectedRow() < 0) {
 					ErrorMessagePane.showErrorMessage(jAdvise, "No student is selected.");
 				} else {
-					AddEditStudent addEditStudent = new AddEditStudent(
-							jAdvise, sd, sd.getStudent((String)
-									table.getModel().getValueAt(
-											table.getSelectedRow(), 0)));
-//					sd.findStudent(TITLE)
-//					String id = (String) table.getModel().getValueAt(table.getSelectedRow(), 0);
-//					System.out.println("ID:" + id);
-//					AddStudent addStudent = new AddStudent(jAdvise, sd, sd.getStudent(sd.findStudent(id)));
+					new AddEditStudent(jAdvise, sd, sd.getStudent(
+							(String) table.getModel().getValueAt(table.getSelectedRow(), 0)
+					));
 				}
 			}
 		});
@@ -204,16 +210,16 @@ public class JAdvise extends JFrame {
 		editMenu.add(editStudentItem);
 		editMenu.add(removeStudentItem);
 
-		//Help Menu
+		// Help Menu
 		aboutItem = new JMenuItem("About");
 		aboutItem.setMnemonic(KeyEvent.VK_A);
-		aboutItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				JOptionPane.showMessageDialog(jAdvise,
-						"Version: 1.0\nDate: 12/15/13\nCreated By David Pirraglia",
-						TITLE, JOptionPane.INFORMATION_MESSAGE);
-			}
+		aboutItem.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(
+					jAdvise,
+					"Version: 1.0\nDate: 12/15/13\nCreated By David Pirraglia",
+					TITLE,
+					JOptionPane.INFORMATION_MESSAGE
+			);
 		});
 
 		helpMenu = new JMenu("Help");
@@ -225,27 +231,12 @@ public class JAdvise extends JFrame {
 		menuBar.add(helpMenu);
 		setJMenuBar(menuBar);
 
-		// Load Data
-		sd = new StudentDatabase(account);
-		try {
-			sd.loadData();
-		} catch (com.mysql.jdbc.exceptions.jdbc4.CommunicationsException ce) {
-			ErrorMessagePane.showErrorMessage(jAdvise, "Can't connect to database.");
-		} catch (SQLException ex) {
-			ErrorMessagePane.showErrorMessage(jAdvise, "SQL Error: " + ex.getMessage());
-			ex.printStackTrace();
-		} catch (ClassNotFoundException ex) {
-			ErrorMessagePane.showErrorMessage(jAdvise,
-					"Something went wrong that you probably won't understand (ClassNotFoundException).");
-			System.exit(1);
-		}
-
 		// Table
 		String[][] rowData = sd.getTableData();
 		table = new JTable(rowData, columnNames) {
 			@Override
 			public boolean isCellEditable(int rowIndex, int colIndex) {
-				return false; //Disallow the editing of any cell
+				return false; // Disallow the editing of any cell
 			}
 		};
 		table.getTableHeader().setReorderingAllowed(false);
@@ -265,6 +256,7 @@ public class JAdvise extends JFrame {
 		});
 
 		// Pressing enter with a row selected will edit it
+		// https://stackoverflow.com/questions/9091208/jtable-enter-key
 		table.getInputMap(JTable.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(
 				KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
 				"\n"
@@ -283,7 +275,6 @@ public class JAdvise extends JFrame {
 		tableScrollPane = new JScrollPane(table);
 		tableScrollPane.getHorizontalScrollBar().setUnitIncrement(15);
 		add(tableScrollPane, BorderLayout.CENTER);
-
 		sd.setTable(table);
 
 		// Search Area
@@ -309,13 +300,10 @@ public class JAdvise extends JFrame {
 		searchPanel.add(searchField, BorderLayout.CENTER);
 		add(searchPanel, BorderLayout.NORTH);
 
-		// Bottom Buttons Area?
 		setVisible(true);
 	}
 
-	public static void resetAllColumnWidths(JTable table) {
-		for (int i = 0; i < columnWidths.length; i++) {
-			table.getColumnModel().getColumn(i).setPreferredWidth(columnWidths[i]);
-		}
+	public static void main(String[] args) {
+		new JAdvise(new MySQLAccount("root", "usbw"));
 	}
 }
