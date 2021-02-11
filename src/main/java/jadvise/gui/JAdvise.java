927f3f32daeac7cba64afb44253a8c9988bf8f46
dapirra <35818038+dapirra@users.noreply.github.com>
2021-02-11 04:05:50 -0500

    Moved the table code higher up so that more lambdas can be used

--- src/main/java/jadvise/gui/JAdvise.java
+++ src/main/java/jadvise/gui/JAdvise.java
@@ -6,7 +6,6 @@ import jadvise.objects.StudentDatabase;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
@@ -113,124 +112,6 @@ public class JAdvise extends JFrame {
 			System.exit(1);
 		}
 
-		// MenuBar
-		menuBar = new JMenuBar();
-
-		// File Menu
-		printItem = new JMenuItem("Print");
-		printItem.setMnemonic(KeyEvent.VK_P);
-		printItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				try {
-					table.print();
-				} catch (PrinterException ex) {
-					ErrorMessagePane.showErrorMessage(jAdvise, "Can't print table.");
-				}
-			}
-		});
-
-		clearItem = new JMenuItem("Clear");
-		clearItem.setMnemonic(KeyEvent.VK_C);
-		clearItem.addActionListener(ae -> {
-			if (JOptionPane.showConfirmDialog(
-					getRootPane(),
-					"Are you sure you want to clear the table?",
-					TITLE,
-					JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
-				try {
-					sd.clearData();
-					sd.updateTable();
-				} catch (SQLException ex) {
-					ex.printStackTrace();
-					System.exit(1);
-				} catch (ClassNotFoundException ex) {
-					System.exit(1);
-				}
-			}
-		});
-
-		exitItem = new JMenuItem("Exit");
-		exitItem.setMnemonic(KeyEvent.VK_X);
-		exitItem.addActionListener(actionEvent -> System.exit(0));
-
-		fileMenu = new JMenu("File");
-		fileMenu.setMnemonic(KeyEvent.VK_F);
-		fileMenu.add(printItem);
-		fileMenu.add(clearItem);
-		fileMenu.add(exitItem);
-
-		// Edit Menu
-		addStudentItem = new JMenuItem("Add Student");
-		addStudentItem.setMnemonic(KeyEvent.VK_A);
-		addStudentItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				new AddEditStudent(jAdvise, sd, null);
-			}
-		});
-
-		editStudentItem = new JMenuItem("Edit Selected Student");
-		editStudentItem.setMnemonic(KeyEvent.VK_E);
-		editStudentItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				if (table.getSelectedRow() < 0) {
-					ErrorMessagePane.showErrorMessage(jAdvise, "No student is selected.");
-				} else {
-					new AddEditStudent(jAdvise, sd, sd.getStudent(
-							(String) table.getModel().getValueAt(table.getSelectedRow(), 0)
-					));
-				}
-			}
-		});
-
-		removeStudentItem = new JMenuItem("Remove Selected Student");
-		removeStudentItem.setMnemonic(KeyEvent.VK_R);
-		removeStudentItem.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent ae) {
-				sd.removeStudent(table.getModel().getValueAt(table.getSelectedRow(), 0) + "");
-				sd.updateTable();
-				try {
-					sd.saveData();
-				} catch (SQLException ex) {
-					ex.printStackTrace();
-					System.exit(1);
-				} catch (ClassNotFoundException ex) {
-					System.exit(1);
-				}
-			}
-		});
-
-		editMenu = new JMenu("Edit");
-		editMenu.setMnemonic(KeyEvent.VK_E);
-		editMenu.add(addStudentItem);
-		editMenu.add(editStudentItem);
-		editMenu.add(removeStudentItem);
-
-		// Help Menu
-		aboutItem = new JMenuItem("About");
-		aboutItem.setMnemonic(KeyEvent.VK_A);
-		aboutItem.addActionListener(actionEvent -> {
-			JOptionPane.showMessageDialog(
-					jAdvise,
-					"Version: 1.0\nDate: 12/15/13\nCreated By David Pirraglia",
-					TITLE,
-					JOptionPane.INFORMATION_MESSAGE
-			);
-		});
-
-		helpMenu = new JMenu("Help");
-		helpMenu.setMnemonic(KeyEvent.VK_H);
-		helpMenu.add(aboutItem);
-
-		menuBar.add(fileMenu);
-		menuBar.add(editMenu);
-		menuBar.add(helpMenu);
-		setJMenuBar(menuBar);
-
 		// Table
 		String[][] rowData = sd.getTableData();
 		table = new JTable(rowData, columnNames) {
@@ -276,6 +157,110 @@ public class JAdvise extends JFrame {
 		tableScrollPane.getHorizontalScrollBar().setUnitIncrement(15);
 		add(tableScrollPane, BorderLayout.CENTER);
 		sd.setTable(table);
+
+		// MenuBar
+		menuBar = new JMenuBar();
+
+		// File Menu
+		printItem = new JMenuItem("Print");
+		printItem.setMnemonic(KeyEvent.VK_P);
+		printItem.addActionListener(actionEvent -> {
+			try {
+				table.print();
+			} catch (PrinterException ex) {
+				ErrorMessagePane.showErrorMessage(jAdvise, "Can't print table.");
+			}
+		});
+
+		clearItem = new JMenuItem("Clear");
+		clearItem.setMnemonic(KeyEvent.VK_C);
+		clearItem.addActionListener(actionEvent -> {
+			if (JOptionPane.showConfirmDialog(
+					getRootPane(),
+					"Are you sure you want to clear the table?",
+					TITLE,
+					JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.WARNING_MESSAGE) == JOptionPane.OK_OPTION) {
+				try {
+					sd.clearData();
+					sd.updateTable();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+					System.exit(1);
+				} catch (ClassNotFoundException ex) {
+					System.exit(1);
+				}
+			}
+		});
+
+		exitItem = new JMenuItem("Exit");
+		exitItem.setMnemonic(KeyEvent.VK_X);
+		exitItem.addActionListener(actionEvent -> System.exit(0));
+
+		fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		fileMenu.add(printItem);
+		fileMenu.add(clearItem);
+		fileMenu.add(exitItem);
+
+		// Edit Menu
+		addStudentItem = new JMenuItem("Add Student");
+		addStudentItem.setMnemonic(KeyEvent.VK_A);
+		addStudentItem.addActionListener(ae -> new AddEditStudent(jAdvise, sd, null));
+
+		editStudentItem = new JMenuItem("Edit Selected Student");
+		editStudentItem.setMnemonic(KeyEvent.VK_E);
+		editStudentItem.addActionListener(actionEvent -> {
+			if (table.getSelectedRow() < 0) {
+				ErrorMessagePane.showErrorMessage(jAdvise, "No student is selected.");
+			} else {
+				new AddEditStudent(jAdvise, sd, sd.getStudent(
+						(String) table.getModel().getValueAt(table.getSelectedRow(), 0)
+				));
+			}
+		});
+
+		removeStudentItem = new JMenuItem("Remove Selected Student");
+		removeStudentItem.setMnemonic(KeyEvent.VK_R);
+		removeStudentItem.addActionListener(actionEvent -> {
+			sd.removeStudent(table.getModel().getValueAt(table.getSelectedRow(), 0) + "");
+			sd.updateTable();
+			try {
+				sd.saveData();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+				System.exit(1);
+			} catch (ClassNotFoundException ex) {
+				System.exit(1);
+			}
+		});
+
+		editMenu = new JMenu("Edit");
+		editMenu.setMnemonic(KeyEvent.VK_E);
+		editMenu.add(addStudentItem);
+		editMenu.add(editStudentItem);
+		editMenu.add(removeStudentItem);
+
+		// Help Menu
+		aboutItem = new JMenuItem("About");
+		aboutItem.setMnemonic(KeyEvent.VK_A);
+		aboutItem.addActionListener(actionEvent -> {
+			JOptionPane.showMessageDialog(
+					jAdvise,
+					"Version: 1.0\nDate: 12/15/13\nCreated By David Pirraglia",
+					TITLE,
+					JOptionPane.INFORMATION_MESSAGE
+			);
+		});
+
+		helpMenu = new JMenu("Help");
+		helpMenu.setMnemonic(KeyEvent.VK_H);
+		helpMenu.add(aboutItem);
+
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+		setJMenuBar(menuBar);
 
 		// Search Area
 		searchLabel = new JLabel(" Search:  ");
