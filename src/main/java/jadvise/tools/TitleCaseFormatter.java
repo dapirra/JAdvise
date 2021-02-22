a3d5908e817f3533e220dab64e3777605ff72ae1
dapirra <35818038+dapirra@users.noreply.github.com>
2021-02-22 04:57:29 -0500

    Wrote multiple different toTitleCase functions and test cases

--- src/main/java/jadvise/tools/TitleCaseFormatter.java
+++ src/main/java/jadvise/tools/TitleCaseFormatter.java
@@ -1,5 +1,7 @@
 package jadvise.tools;
 
+import java.util.stream.Stream;
+
 /**
  * @author David Pirraglia
  */
@@ -15,5 +17,44 @@ public class TitleCaseFormatter {
 			}
 		}
 		return output.trim();
+	}
+
+	public static String toTitleCase2(String input) {
+		String[] array = input.split(" ");
+		StringBuilder output = new StringBuilder();
+		for (String i : array) {
+			if (i.length() > 1) {
+				output.append(i.substring(0, 1).toUpperCase())
+						.append(i.substring(1).toLowerCase());
+			} else {
+				output.append(i.toUpperCase());
+			}
+			output.append(" ");
+		}
+		return output.substring(0, output.length() - 1);
+	}
+
+	public static String toTitleCase3(String input) {
+		String[] array = input.split(" ");
+		for (int i = 0; i < array.length; i++) {
+			if (array[i].length() > 1) {
+				array[i] = array[i].substring(0, 1).toUpperCase()
+						+ array[i].substring(1).toLowerCase();
+			} else {
+				array[i] = array[i].toUpperCase();
+			}
+		}
+		return String.join(" ", array);
+	}
+
+	public static String toTitleCase4(String input) {
+		return String.join(" ", Stream.of(input.split(" ")).map(i -> {
+			if (i.length() > 1) {
+				return i.substring(0, 1).toUpperCase() +
+						i.substring(1).toLowerCase();
+			} else {
+				return i.toUpperCase();
+			}
+		}).toArray(String[]::new));
 	}
 }
